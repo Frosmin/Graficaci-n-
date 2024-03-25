1df8672306e3281eaaf4d09c9558ca4e34f8a1de
unknown <ricardo.rojas.carvajal@gmail.com>
2024-03-25 18:48:08 -0300

    asdf

--- algoritmos.py
+++ algoritmos.py
@@ -3,36 +3,36 @@
 import tkinter as tk
 import math
 import time 
-
+from multiprocessing import Process
+import threading
 
 
 class Gui:
     def __init__(self, master) -> None:
         self.master = master
         
+        self.pixel_size = 10
+        self.grid_size = 50
+
         # Crear un lienzo (canvas)
-        self.canvas = tk.Canvas(self.master, width=1000, height=700)
+        self.canvas = tk.Canvas(self.master, width=self.grid_size*self.pixel_size, height=self.grid_size*self.pixel_size)
         self.canvas.pack()
         self.canvas.config(bg="white")
 
-      
-        # self.draw_line_basic(200,200,100,1)
-        # print("")
-        # self.draw_line_dda(200,200,100,1)
-        # print("")
-        # self.draw_line_bresenham(200,200,100,1)
-        
-        # print("")
-        self.draw_circle_basic(200, 200, 100) #basico 
-        print("")
-        # self.draw_circle_polar(200,200,100)
-        # print("")
-        #self.draw_circle_bresenham(200,200,100)
-        
+        self.draw_grid()
+
+    
+    def draw_grid(self):
+        for i in range(self.grid_size):
+            for j in range(self.grid_size):
+                x1, y1 = (i*self.pixel_size, j*self.pixel_size)
+                x2, y2 = ((i+1)*self.pixel_size, (j+1)*self.pixel_size)
+                self.canvas.create_rectangle(x1, y1, x2, y2, outline="gray", width=1)
+    
     def draw_pixel(self, x, y):
-        x1, y1 = (x,y)
-        x2, y2 = (x + 1, y + 1)
-        self.canvas.create_rectangle(x1, y1, x2, y2)
+        x1, y1 = (x*self.pixel_size,y*self.pixel_size)
+        x2, y2 = ((x+1)*self.pixel_size, (y+1)*self.pixel_size)
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill="black")
 
     def draw_line_basic(self, x0, y0, x1, y1):
         begin = time.time()
@@ -52,6 +52,8 @@ class Gui:
                 y = m * x + b
                 self.draw_pixel(x, y)
                 x -= 1
+
+
         end = time.time()
         print(begin)
         print(end)
@@ -81,6 +83,8 @@ class Gui:
             x += x_increment
             y += y_increment
             end = time.time()
+            self.master.update()
+            time.sleep(0.5)
         print(begin)
         print(end)
         print(f"timpo de ejecucion linea DDA: {end - begin}")
@@ -88,8 +92,8 @@ class Gui:
     
     def draw_line_bresenham(self, x0, y0, x1, y1):
         begin = time.time()
-        dx = abs(x1 - x0)
-        dy = abs(y1 - y0)
+        dx = abs(x1 - x0) 
+        dy = abs(y1 - y0) 
         sx = -1 if x0 > x1 else 1
         sy = -1 if y0 > y1 else 1
         err = dx - dy
@@ -98,11 +102,15 @@ class Gui:
             self.draw_pixel(x0, y0)
             e2 = 2 * err
             if e2 > -dy:
-                err -= dy
-                x0 += sx
+                err -= (dy)
+                x0 += (sx)
             if e2 < dx:
-                err += dx
-                y0 += sy
+                err += (dx)
+                y0 += (sy)
+            self.master.update()
+            time.sleep(0.5)
+            
+
         end = time.time()
         print(begin)
         print(end)
@@ -155,16 +163,39 @@ class Gui:
             else:
                 y -= 1
                 p = p + 2 * (x - y) + 1
+            self.master.update()
+            time.sleep(1)
+
         end = time.time()
         print(f"Tiempo de inicio: {begin}")
         print(f"Tiempo de fin: {end}")
         print(f"Tiempo de ejecución circunferencia Bresenham: {end - begin}")
 
     
-root = tk.Tk()
+def crear_interfaz(name,funcion,titulo,*arg):
+    name = tk.Tk()
+    my_gui = Gui(name)
+    name.title(titulo)
+    if funcion == "bres":
+        x,y,z = arg
+        my_gui.draw_circle_bresenham(x,y,z)
+    elif funcion == "polar" :
+        x,y,z = arg
+        my_gui.draw_circle_polar(x,y,z)
+    elif funcion == "dda":
+        x,y,z,w = arg
+        my_gui.draw_line_dda(x,y,z,w)
+    else:    
+        x,y,z,w = arg
+        my_gui.draw_line_bresenham(x,y,z,w)
+    name.mainloop()
 
-root.title("Algoritmos para graficacion")   
+if __name__ == "__main__":
+    t1 = Process(target=crear_interfaz, args=("root1","dda","Algoritmo dda linea",10,10,40,35))
+    t2 = Process(target=crear_interfaz, args=("root2","asdf","Algoritmo b linea",10,10,40,35))
 
-my_gui = Gui(root)
+    t1.start()
+    t2.start()
 
-root.mainloop()
+    t1.join()
+    t2.join()
