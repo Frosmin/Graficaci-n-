bd5349ca04a102f330453a1fd378259c17788a1b
unknown <ricardo.rojas.carvajal@gmail.com>
2024-04-12 20:19:53 -0300

    agregando funcionalidad de colores

--- algoritmos.py
+++ algoritmos.py
@@ -6,6 +6,9 @@ import math
 import time 
 from PIL import Image, ImageTk
 
+
+
+
 class Gui:
     def __init__(self, master) -> None:
         self.master = master
@@ -68,11 +71,8 @@ class Gui:
         self.canvas.bind('<Button-1>', self.handle_click)
 
         
-
     def color_picker(self):
-        my_color = colorchooser.askcolor()[0]
-        self.color.set(my_color)
-        print(self.color.get())
+        self.color.set(colorchooser.askcolor()[1])
     
     def set_linea(self):
         self.puntos.clear()
@@ -83,7 +83,6 @@ class Gui:
         self.puntos.clear()
         self.es_linea = False
         self.es_circulo = True
-
 
     def handle_click(self, event = None):
         x , y = int(event.x) , int(event.y)
@@ -103,7 +102,7 @@ class Gui:
 
     def draw_line(self):
         linea = self.Line(self.canvas) 
-        linea.draw_line_bresenham(*self.puntos[-1],*self.puntos[-2])
+        linea.draw_line_bresenham(*self.puntos[-1],*self.puntos[-2], color=self.color.get()) 
         self.is_drawing=False
     
     def draw_circle(self):
@@ -111,24 +110,21 @@ class Gui:
             r = round(math.sqrt((x2-x1)**2 + (y2-y1)**2))
             return r
         circulo = self.Circle(self.canvas)
-        circulo.draw_circle_bresenham(*self.puntos[0], calc_r(*self.puntos[0], *self.puntos[1]))
-
+        circulo.draw_circle_bresenham(*self.puntos[0], calc_r(*self.puntos[0], *self.puntos[1]), color=self.color.get())
 
     class Line():
         def __init__(self, canva) -> None:
             self.canvas = canva
 
-        def draw_pixel(self, x, y):
-    
+        def draw_pixel(self, x, y, color="black"):
             x1, y1 = (x,y)
             x2, y2 = ((x+1), (y+1))
-            self.canvas.create_rectangle(x1, y1, x2, y2, fill="black")
-            
+            self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, width=0)        
+                
         def draw_line_basic(self, x0, y0, x1, y1):
-            begin = time.time()
             m = (y1 - y0)/(x1 - x0)       
             b = y0 - m * x0
-            
+                
             self.draw_pixel(x0, y0)
             x = x0
             y = y0
@@ -143,47 +139,34 @@ class Gui:
                     self.draw_pixel(x, y)
                     x -= 1
 
-            end = time.time()
-            print(begin)
-            print(end)
-            print(f"timpo de ejecucion linea basico: {end - begin}")
-
         def draw_line_dda(self, x0, y0, x1, y1):
             begin = time.time()
             dx = x1 - x0
             dy = y1 - y0
-
             if abs(dx) > abs(dy):
                 steps = abs(dx)
             else:
                 steps = abs(dy)
-
             x_increment = dx / steps
             y_increment = dy / steps
-
             x = x0
             y = y0
-
             for i in range(steps):
                 self.draw_pixel(round(x), round(y))
                 x += x_increment
                 y += y_increment
                 end = time.time()
                 self.master.update()
-                time.sleep(0.02)
-            print(begin)
-            print(end)
-            print(f"timpo de ejecucion linea DDA: {end - begin}")
+                
 
-        def draw_line_bresenham(self, x0, y0, x1, y1):
+        def draw_line_bresenham(self, x0, y0, x1, y1, color="black"):
             dx = abs(x1 - x0) 
             dy = abs(y1 - y0) 
             sx = -1 if x0 > x1 else 1
             sy = -1 if y0 > y1 else 1
             err = dx - dy
-
             while x0 != x1 or y0 != y1:
-                self.draw_pixel(x0, y0)
+                self.draw_pixel(x0, y0, color=color)
                 e2 = 2 * err
                 if e2 > -dy:
                     err -= (dy)
@@ -191,62 +174,46 @@ class Gui:
                 if e2 < dx:
                     err += (dx)
                     y0 += (sy)
-
-    
+                
+       
     class Circle():
         def __init__(self, canva) -> None:
             self.canvas = canva
 
-        def draw_pixel(self, x, y):
-            #x1, y1 = (x*self.pixel_size,y*self.pixel_size)
-            #x2, y2 = ((x+1)*self.pixel_size, (y+1)*self.pixel_size)
+        def draw_pixel(self, x, y, color="black"):
             x1, y1 = (x,y)
             x2, y2 = ((x+1), (y+1))
-            self.canvas.create_rectangle(x1, y1, x2, y2, fill="black")
+            self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, width=0)
 
         def draw_circle_basic(self, xc, yc, r):
-            begin = time.time()
             for x in range(xc - r, xc + r ):  #200 200 100      100, 100   
                 y_positivo = yc + int((r ** 2 - (x - xc) **  2) ** 0.5)
                 y_negativo = yc - int((r ** 2 - (x - xc) ** 2) ** 0.5)
                 self.draw_pixel(x, y_positivo)
                 self.draw_pixel(x, y_negativo)
-                self.master.update()
-                time.sleep(0.1)
-            end = time.time()
-            print(begin)
-            print(end)
-            print(f"Tiempo de ejecución circunferencia basico: {end - begin}")
 
         def draw_circle_polar(self, xc, yc, r):
-            begin = time.time()
             for i in range(360):
                 sin_values = r * math.sin(i * math.pi / 180)
                 cos_values = r * math.cos(i * math.pi / 180) 
                 x = xc + cos_values
                 y = yc + sin_values
-                self.draw_pixel(round(x), round(y))
-                self.master.update()
-                time.sleep(0.02)    
-            end = time.time()
-            print(f"Tiempo de inicio: {begin}")
-            print(f"Tiempo de fin: {end}")
-            print(f"Tiempo de ejecución circunferencia polar: {end - begin}")
-
-        def draw_circle_bresenham(self, x_center, y_center, r):
+                self.draw_pixel(round(x), round(y))  
+  
+        def draw_circle_bresenham(self, x_center, y_center, r, color="black"):
             x = 0
             y = r
             p = 1 - r
 
             while x <= y:
-                self.draw_pixel(x_center + x, y_center + y)
-                self.draw_pixel(x_center - x, y_center + y)
-                self.draw_pixel(x_center + x, y_center - y)
-                self.draw_pixel(x_center - x, y_center - y)
-                self.draw_pixel(x_center + y, y_center + x)
-                self.draw_pixel(x_center - y, y_center + x)
-                self.draw_pixel(x_center + y, y_center - x)
-                self.draw_pixel(x_center - y, y_center - x)
+                self.draw_pixel(x_center + x, y_center + y, color=color)
+                self.draw_pixel(x_center - x, y_center + y, color=color)
+                self.draw_pixel(x_center + x, y_center - y, color=color)
+                self.draw_pixel(x_center - x, y_center - y, color=color)
+                self.draw_pixel(x_center + y, y_center + x, color=color)
+                self.draw_pixel(x_center - y, y_center + x, color=color)
+                self.draw_pixel(x_center + y, y_center - x, color=color)
+                self.draw_pixel(x_center - y, y_center - x, color=color)
 
                 x += 1
                 if p <= 0:
